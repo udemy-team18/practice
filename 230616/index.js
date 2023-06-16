fc3e1d095d122942ea528b80c8f206f13b39d80d
seungji <dkstmdwl0615@naver.com>
2023-06-16 11:50:09 +0900

    js using dom modification, diff with getElement and querySelector

---
+++ 230616/index.js
@@ -0,0 +1,24 @@
+const person = {
+  firstName: "john",
+  lastName: "doe",
+  age: 18,
+  team: 18,
+};
+
+console.log(person.team);
+
+const wrapper = document.getElementById("wrapper");
+
+const name1 = document.getElementsByClassName("first_name");
+const name2 = document.getElementsByClassName("last_name").innerHTML;
+
+const name3 = document.querySelector(".first_name");
+
+console.log(typeof name1);
+console.log(name1);
+console.log(name3);
+
+// wrapper.innerHTML = person.firstName + name1 + name2;
+
+name1.innerHTML = person.firstName;
+name3.innerHTML = person.firstName;
