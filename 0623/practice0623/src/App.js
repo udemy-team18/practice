1d233eec688ad03c238eb4625ab220a23351f55e
yueunkim <yueunkim0605@gmail.com>
2023-06-27 13:16:34 +0900

    0623실습

--- 0623/practice0623/src/App.js
+++ 0623/practice0623/src/App.js
@@ -1,13 +1,15 @@
-import Todo from "./components/TodoList";
-import Random from "./components/Random";
-import Slider from "./components/Slider";
+// import Todo from "./components/TodoList";
+// import Random from "./components/Random";
+// import Slider from "./components/Slider";
+import Weathers from "./components/Weather";
 
 function App() {
   return (
     <>
-      <Todo />
-      <Random min={1} max={100} />
-      <Slider />
+      {/* <Todo /> */}
+      {/* <Random min={1} max={100} /> */}
+      {/* <Slider /> */}
+      <Weathers />
     </>
   );
 }
