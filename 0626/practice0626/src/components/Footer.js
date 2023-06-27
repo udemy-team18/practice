13f51e3e82ddafadea6ddaae2465d20bef2eb0c0
Yueun Kim <65431814+YueunKim@users.noreply.github.com>
2023-06-27 16:51:14 +0900

    Delete 0626/practice0626 directory

--- 0626/practice0626/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles/css/Navigation.module.css";
-
-const Footer = () => {
-  return <footer className={styles.footer}>Footer 영역</footer>;
-};
-
-export default Footer;
