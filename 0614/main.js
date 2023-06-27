ebc653cf83831ea7cc6ff47cd3f0089c6aa5d225
Yueun Kim <65431814+YueunKim@users.noreply.github.com>
2023-06-27 16:49:55 +0900

    Delete 0614 directory

--- 0614/main.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector(".navbar-toggle");
-const menu = document.querySelector(".navbar-menu");
-const login = document.querySelector(".navbar-login");
-
-toggle.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  login.classList.toggle("active");
-});
