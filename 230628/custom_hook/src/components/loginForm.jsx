669c3899c59d5fe874faf96d2de09cb82c935c74
seungji <dkstmdwl0615@naver.com>
2023-06-28 13:56:36 +0900

    프로젝트 설명회 전 실습 코드 붙여넣기 완료

--- 230628/custom_hook/src/components/loginForm.jsx
+++ 230628/custom_hook/src/components/loginForm.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import useForm from "../hooks/useForm";
 
-export default function Loginform() {
-  const { value, handleChange, resetForm } = useForm({
-    userName: "",
+export default function LoginForm() {
+  const { values, handleChange, resetForm } = useForm({
+    username: "",
     password: "",
   });
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("login data");
+    console.log("로그인 데이터", values);
     resetForm();
   };
-
-  return <form></form>;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>사용자명 : </label>
+        <input
+          type="text"
+          name="username"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>비밀번호 : </label>
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">로그인</button>
+    </form>
+  );
 }
